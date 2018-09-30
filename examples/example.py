8091faac744aa786cc1cbbf70b60f4264f55fd63
YomnaJehad <yomnaj96@gmail.com>
2018-09-30 15:16:29 +0000

    First

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,6 @@
 def warper(img, src, dst):
 
-    # Compute and apply perpective transform
+    # Compute and apply perspective transform
     img_size = (img.shape[1], img.shape[0])
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
